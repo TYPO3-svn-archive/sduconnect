e4c9fe4de050d587183731a17c287d60a4f36eda
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-09-29 14:19:53 +0000

    Initial import
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sduconnect/trunk@12424 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "ooip_sduconnectloket"
+#
+# Auto generated 03-09-2008 11:09
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'SDU Connect',
+	'description' => 'Connects to the Dutch database for municipalities and shows information from that database',
+	'category' => 'plugin',
+	'author' => 'Dmitry Dulepov [Netcreators]',
+	'author_email' => 'dmitry.dulepov@gmail.com',
+	'shy' => 0,
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => 'Netcreators BV',
+	'version' => '0.1.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"db1a";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"8881";s:17:"ext_localconf.php";s:4:"aab5";s:14:"ext_tables.php";s:4:"cab1";s:19:"flexform_ds_pi1.xml";s:4:"239c";s:13:"locallang.xml";s:4:"aad1";s:16:"locallang_db.xml";s:4:"a6cc";s:14:"doc/manual.sxw";s:4:"1cd3";s:14:"pi1/ce_wiz.gif";s:4:"ae5d";s:40:"pi1/class.tx_ooipsduconnectloket_pi1.php";s:4:"6e7d";s:48:"pi1/class.tx_ooipsduconnectloket_pi1_wizicon.php";s:4:"f105";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"262e";}',
+	'suggests' => array(
+	),
+);
+
+?>
