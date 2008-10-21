440d0b94665af9329bb4edfa6ebb800907e10a6d
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-10-21 08:02:05 +0000

    0.4.3
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sduconnect@13193 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ tags/0_4_3/ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "sduconnect"
+#
+# Auto generated 17-10-2008 15:38
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'SDU Connect',
+	'description' => 'Connects to the Dutch database for municipalities and shows information from that database',
+	'category' => 'plugin',
+	'author' => 'Dmitry Dulepov [Netcreators]',
+	'author_email' => 'dmitry@netcreators.com',
+	'shy' => 0,
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => 'Netcreators BV',
+	'version' => '0.4.3',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"fdca";s:12:"ext_icon.gif";s:4:"8881";s:17:"ext_localconf.php";s:4:"50eb";s:14:"ext_tables.php";s:4:"12a2";s:24:"ext_typoscript_setup.txt";s:4:"fceb";s:19:"flexform_ds_pi1.xml";s:4:"d663";s:13:"locallang.xml";s:4:"b76e";s:16:"locallang_db.xml";s:4:"d226";s:14:"doc/manual.sxw";s:4:"1cd3";s:14:"pi1/ce_wiz.gif";s:4:"ae5d";s:31:"pi1/class.tx_sduconnect_pi1.php";s:4:"9f3f";s:39:"pi1/class.tx_sduconnect_pi1_wizicon.php";s:4:"af97";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"146a";}',
+	'suggests' => array(
+	),
+);
+
+?>
